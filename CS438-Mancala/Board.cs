9827653efc3255e20345c4d6d3abff9c2610db23
Andrew Milloshewski <amillos@siue.edu>
2022-04-21 00:00:45 -0500

    added makeMove

--- CS438-Mancala/Board.cs
+++ CS438-Mancala/Board.cs
@@ -46,7 +46,41 @@ namespace CS438_Mancala
 
         public void makeMove(int pocket)
         {
-            
+            if (playerTurn == 1) {
+                pocket--;
+            }
+            int stones = gameState[playerTurn, pocket];
+            int row = playerTurn;
+            int col = pocket;
+            for(int i = 0; i < stones; i++) {
+                if (playerTurn == 0) {
+                    if (col == 5 && row == 1) {
+                        row--;
+                        col++;
+                    } else if (col == 0 && row == 0)
+                        row++;
+                    else if (row == 0)
+                        col--;
+                    else if (row == 1)
+                        col++;
+
+                    gameState[row, col] += 1;
+                }
+
+                if (playerTurn == 1) {
+                    if (col == 1 && row == 0) {
+                        row++;
+                        col--;
+                    } else if (col == 6 && row == 1)
+                        row--;
+                    else if (row == 1)
+                        col++;
+                    else if (row == 0)
+                        col--;
+
+                    gameState[row, col] += 1;
+                }
+            }
 
         }
     }

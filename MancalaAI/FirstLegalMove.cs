d469cbc6dda5633b79dfd39f04ec5d57a7e3d5a0
Michael French <michael.x.french@gmail.com>
2022-04-24 18:01:13 -0500

    added a project for the AI

---
+++ MancalaAI/FirstLegalMove.cs
@@ -0,0 +1,11 @@
+﻿// Hello World! program
+namespace MancalaAI
+{
+    class FirstLegalMove
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}

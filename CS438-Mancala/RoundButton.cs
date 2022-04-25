3683782d3135299a87bc408aaae4d5cfb0ea2601
McMule2020 <61675001+McMule2020@users.noreply.github.com>
2022-04-24 23:40:35 -0500

    Added circle buttons that change highlights on hover.

---
+++ CS438-Mancala/RoundButton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace CS438_Mancala
+{
+    class RoundButton : Button
+    {
+        protected override void OnPaint(System.Windows.Forms.PaintEventArgs e)
+        {
+            GraphicsPath grPath = new GraphicsPath();
+            grPath.AddEllipse(10, 10, ClientSize.Width - 20, ClientSize.Height - 20);
+            this.Region = new System.Drawing.Region(grPath);
+            base.OnPaint(e);
+        }
+    }
+}
